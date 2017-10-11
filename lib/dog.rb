610338e354020e89f2ec75519358a20d3b630602
Joshua Needham <joshuabneedham@gmail.com>
2017-10-11 18:02:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,22 @@
 class Dog
-  def initialize(name, bark)
-    @name = name
-    @bark = bark
+  def initialize(name)
+    @name = new_name
   end
 
   def name=(new_name)
     @name = new_name
   end
 
+  def name
+    "#{@name}"
+  end
+
   def bark=(bark)
     @bark = puts "woof!"
   end
+
+  def bark
+    "#{@bark}"
+  end
+  
 end
